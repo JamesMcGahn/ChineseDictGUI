0287ea65d8e646f1dc3f6e97e88fad2d83282d38
James McGahn <jmcgahn@gmail.com>
2024-09-04 22:00:28 -0400

    rewrite network thread as thread and worker

--- gui/network_thread.py
+++ gui/network_thread.py
@@ -1,38 +1,89 @@
 import requests
-from PySide6.QtCore import QThread, Signal
+from PySide6.QtCore import QObject, QThread, Signal, Slot
 
 
-class NetworkThread(QThread):
+class NetworkWorker(QObject):
+    finished = Signal()
     response_sig = Signal(str, object)
-    error_sig = Signal(str, str)
+    error_sig = Signal(str, str, str)
 
-    def __init__(self, session_mangager, operation, url, data=None):
+    def __init__(self, session_mangager, operation, url, data=None, json=None):
         super().__init__()
         self.session_mangager = session_mangager
         self.url = url
         self.data = data
         self.operation = operation
+        self.json = json
 
-    def run(self):
+    @Slot()
+    def do_work(self):
         try:
             if self.operation == "POST":
                 response = self.session_mangager.post(self.url, data=self.data)
 
             elif self.operation == "SESSION":
-                response = self.session_mangager.post(self.url, data=self.data)
+                response = self.session_mangager.post(
+                    self.url, data=self.data, json=self.json
+                )
                 if not any(c.name == "lang" for c in response.cookies):
                     raise requests.exceptions.RequestException
-                print("here1", response)
-                print("here", response.text)
+
+            elif self.operation == "GET":
+                response = self.session_mangager.get(
+                    self.url, data=self.data, json=self.json
+                )
 
             if response.status_code in (200, 201):
-                print("suc", response)
                 self.response_sig.emit("success", response)
             else:
                 self.error_sig.emit("success", response)
                 print("suc2", response)
 
-        except requests.exceptions.RequestException as e:
+        except requests.exceptions.ConnectionError as e:
             print("eerr")
-            print(e)
-            self.error_sig.emit("error", str(e))
+
+            self.error_sig.emit("error", str(e), type(e).__name__)
+
+        except requests.exceptions.RequestException as e:
+            print("eerrs")
+
+            self.error_sig.emit("error", str(e), type(e).__name__)
+
+        finally:
+            self.finished.emit()
+
+
+class NetworkThread(QThread):
+    response_sig = Signal(str, object)
+    error_sig = Signal(str, str)
+    finished = Signal()
+
+    def __init__(self, session_mangager, operation, url, data=None, json=None):
+        super().__init__()
+        self.session_mangager = session_mangager
+        self.url = url
+        self.data = data
+        self.operation = operation
+        self.json = json
+
+    def run(self):
+        self.worker = NetworkWorker(
+            self.session_mangager, self.operation, self.url, self.data, self.json
+        )
+        self.worker.moveToThread(self)
+        self.worker.finished.connect(self.quit)
+        self.worker.finished.connect(self.finished)
+
+        self.worker.response_sig.connect(self.received_response)
+        self.worker.error_sig.connect(self.received_error)
+        self.worker.do_work()
+
+    def finished(self):
+        self.worker.deleteLater()
+        self.finished.emit()
+
+    def received_response(self, status, response):
+        self.response_sig.emit(status, response)
+
+    def received_error(self, status, err):
+        self.error_sig.emit(status, err)
