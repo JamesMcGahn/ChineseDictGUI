bafe29c7a7e0bb6d248d51240c04858146b1eb69
James McGahn <jmcgahn@gmail.com>
2025-06-06 17:16:35 -0400

    find the menu on the page to see what the lesson actually has, explicitly wait for section panels

--- core/scrapers/cpod/lessons/web_scrape.py
+++ core/scrapers/cpod/lessons/web_scrape.py
@@ -1,10 +1,13 @@
 from time import sleep
 
+from PySide6.QtCore import QMutexLocker, QObject, Signal
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 from keys import keys
@@ -12,9 +15,10 @@ from services import Logger
 
 
 class WebScrape:
+
     def __init__(self, session, url):
         chrome_options = Options()
-        chrome_options.add_argument("--headless=new")
+        # chrome_options.add_argument("--headless=new")
         self.session = session
         self.cookies = session.get_cookies()
         self.driver = webdriver.Chrome(
@@ -58,6 +62,25 @@ class WebScrape:
             self.driver.get(url)
             sleep(3)
 
+            menu_list = self.driver.find_element(
+                By.CSS_SELECTOR, "div.cpod-card-top ul.lesson-menu-tabs"
+            )
+            tabs = menu_list.find_elements(By.TAG_NAME, "a")
+            tabs_titles = [tab.get_attribute("title") for tab in tabs]
+            print(tabs_titles)
+            tab_titles = [
+                tab
+                for tab in tabs_titles
+                if tab in ("Dialogue", "Vocabulary", "Expansion", "Grammar")
+            ]
+
+            self.not_available = [
+                na
+                for na in ("Dialogue", "Vocabulary", "Expansion", "Grammar")
+                if na not in tab_titles
+            ]
+            print("not available", self.not_available)
+
             def wait_for_content(content):
                 ids = {
                     "Dialogue": "dialogue",
@@ -65,14 +88,29 @@ class WebScrape:
                     "Expansion": "expansion",
                     "Grammar": "lesson-grammar",
                 }
-                self.driver.implicitly_wait(120)
-                self.driver.find_element(By.ID, ids[content])
 
-            for link in ("Dialogue", "Vocabulary", "Expansion", "Grammar"):
+                target_id = ids.get(content)
+                if not target_id:
+                    return
+                WebDriverWait(self.driver, 120).until(
+                    EC.presence_of_element_located((By.ID, target_id))
+                )
+
+                WebDriverWait(self.driver, 120).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, f"#{target_id} *"))
+                )
+
+                if target_id == "lesson-vocabulary":
+                    WebDriverWait(self.driver, 40).until(
+                        EC.presence_of_element_located((By.ID, "key_vocab"))
+                    )
+
+            for link in tab_titles:
                 try:
                     self.driver.find_element(
                         By.CSS_SELECTOR, f"a[title='{link}']"
                     ).click()
+                    sleep(2)
                     wait_for_content(link)
                     sleep(5)
                 except NoSuchElementException as e:
@@ -86,3 +124,4 @@ class WebScrape:
         except Exception as e:
             Logger().insert("Something Went Wrong...", "ERROR")
             Logger().insert(e, "ERROR", False)
+            print(e)
