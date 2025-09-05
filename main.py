d42e849dbe04b453a099025e328cd366c0edacd1
James McGahn <jmcgahn@gmail.com>
2025-09-05 15:14:08 -0400

    update requirements txt, pipfile, main startup fn

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import sys
 
+from PySide6.QtCore import QCoreApplication, Qt
+from PySide6.QtGui import QGuiApplication
 from PySide6.QtWidgets import QApplication
 
 from views import MainWindow
 
-app = QApplication(sys.argv)
 
-window = MainWindow(app)
-window.show()
+def main():
 
-app.exec()
+    QCoreApplication.setOrganizationName("ChineseDictGUI")
+    QCoreApplication.setApplicationName("ChineseDictGUI")
+    QCoreApplication.setApplicationVersion("0.1.0")
+
+    app = QApplication(sys.argv)
+    window = MainWindow(app)
+    window.show()
+    app.exec()
+
+    QGuiApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
+    )
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
