bc43345e015042ae14144fc14776b80caa43d21d
James McGahn <jmcgahn@gmail.com>
2025-11-27 13:58:57 -0500

    add remove files worker

--- utils/files/__init__.py
+++ utils/files/__init__.py
@@ -1,5 +1,6 @@
 from .open_file import OpenFile
 from .path_manager import PathManager
+from .remove_files_worker import RemoveFileWorker
 from .write_file import WriteFile
 
-__all__ = ["OpenFile", "WriteFile", "PathManager"]
+__all__ = ["OpenFile", "WriteFile", "PathManager", "RemoveFileWorker"]
