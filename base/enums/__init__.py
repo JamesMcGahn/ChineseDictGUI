28c136e7ceb026c3c76fc4f1312b2a63a1827b76
James McGahn <jmcgahn@gmail.com>
2025-12-14 14:18:07 -0500

    add lesson level enums

--- base/enums/__init__.py
+++ base/enums/__init__.py
@@ -1,3 +1,4 @@
+from .lesson_level import LESSONLEVEL
 from .log_level import LOGLEVEL
 
-__all__ = ["LOGLEVEL"]
+__all__ = ["LOGLEVEL", "LESSONLEVEL"]
---
+++ base/enums/lesson_level.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class LESSONLEVEL(Enum):
+    NEWBIE = "Newbie"
+    ELEMENTARY = "Elementary"
+    PRE_INTERMEDIATE = "Pre-Intermediate"
+    INTERMEDIATE = "Intermediate"
+    ADVANCED = "Advanced"
+    MEDIA = "Media"
